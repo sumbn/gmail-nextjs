0000a6d96fec4d6919d810ce56046a1090a298a2
sum bùi <sumbn37k02.1@gmail.com>
2024-08-26 18:14:39 +0700

    update name

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,16 +2,18 @@
 
 import { signOut } from "next-auth/react"
 import Link from "next/link"
+import {useSession} from "next-auth/react"
 
 const logout = () => {
   signOut({callbackUrl:'/login'})
 }
 
 const Header = () => {
+  const {data: session} = useSession()
   return (
     <header className="bg-gray-800 text-white py-4 px-4 md:px-8">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">Welcome</Link>
+        <Link href="/" className="text-2xl font-bold">Welcome {session?.user.name}</Link>
         <nav>
           <ul className="flex space-x-6">
             <li><Link className="hover:text-gray-400 transition" href="/">Home</Link></li>
